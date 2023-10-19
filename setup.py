cdf4adec0c1249f7aa08672201145a0a89a977f9
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-10-19 09:22:42 -0500

    Correct package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='my_package',
+    name='mk_ic',
     version='0.1',
     packages=find_packages(),
     install_requires=requirements,
