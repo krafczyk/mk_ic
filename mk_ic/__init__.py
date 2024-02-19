c0afa78671838482613c869dd38377c81e40e3f2
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2024-02-19 14:21:25 -0600

    Add new functions to __init__.py

--- mk_ic/__init__.py
+++ mk_ic/__init__.py
@@ -1,9 +1,11 @@
-from mk_ic.main import install, setup
+from mk_ic.main import install, setup, install_func, remove_func
 from mk_ic.utils import pytest_wrapper_elimination
 
 
 __all__ = [
     install,
+    install_func,
+    remove_func,
     setup,
     pytest_wrapper_elimination
 ]
